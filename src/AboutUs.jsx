94856b9f0cd7f072705295ce6dc4c99e781438f6
vibin-babu <vibinpv71@gmail.com>
2026-05-02 02:10:37 -0400

    pages updated

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,19 +5,14 @@ function AboutUs() {
   return (
     <div className="about-us-container">
       {/* <h1 className="about-us-heading">About Us</h1> */}
-      <p className="about-us-description">Welcome to Paradise Nursery, where green meets serenity!</p>
+      <p className="about-us-description">Welcome to Green Haven Nursery, where green meets serenity!</p>
       <p className="about-us-content">
-        At Paradise Nursery, we are passionate about bringing nature closer to you. Our mission is to provide a wide range of 
-        high-quality plants that not only enhance the beauty of your surroundings but also contribute to a healthier and 
-        more sustainable lifestyle. From air-purifying plants to aromatic fragrant ones, we have something for every 
-        plant enthusiast.
+        where nature meets comfort. We are dedicated to bringing fresh,
+         vibrant greenery into your everyday spaces. Our collection features a wide variety of carefully selected plants that not only enhance your home or office but also promote a healthier and more peaceful environment.
       </p>
       {/* <p className="plant_logo_left"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
       <p className="about-us-content">
-        Our team of experts is dedicated to ensuring that each plant meets our strict standards of quality and care. 
-        Whether you're a seasoned gardener or just starting your green journey, we're here to support you every step of 
-        the way. Feel free to explore our collection, ask questions, and let us help you find the perfect plant for your 
-        home or office.
+        Whether you're a beginner or an experienced plant lover, we make plant care simple and enjoyable. At Paradise Nursery, quality, sustainability, and customer satisfaction are at the heart of everything we do. Let us help you create your own green sanctuary.
       </p>
       {/* <p className="plant_logo_right"><img src="https://p1.hiclipart.com/preview/922/979/640/green-leaf-logo-emoji-seedling-emoticon-sticker-plant-plant-stem-flower-png-clipart-thumbnail.jpg" height='50px' width='50px' alt="" /></p> */}
 
